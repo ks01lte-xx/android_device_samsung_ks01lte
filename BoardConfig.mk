b24a70e4f7d77b04ccafedc2e136d6e608ba3191
1bdellatif <ptaoubi@gmail.com>
2024-09-12 20:48:34 +0100

    msm8974-common: TARGET_KERNEL_CLANG_COMPILE := false
    Needed after
    https://github.com/LineageOS/android_vendor_lineage/commit/6d88852d2e602a92dde7bea849ff614f6a5165b1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 zcache.enabled=1 zcache.compressor=lz4
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+TARGET_KERNEL_CLANG_COMPILE := false
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
