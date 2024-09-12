2bb6f35ba5ab2cd995766c69bf6925235d26f8b4
1bdellatif <ptaoubi@gmail.com>
2024-09-12 21:46:12 +0100

    msm8974-common: Use default keymaster@4.1 HIDL
    * We use software keymaster anyway, let's avoid letting keymaster HIDL down at every boot seeing that a secure keystore doesn't exists
    
    Change-Id: Ic597f98dfec91e476e08a0b9fc7d9a59da7084e1

--- device.mk
+++ device.mk
@@ -127,8 +127,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@4.1-service
 
 PRODUCT_PACKAGES += \
     libkeymaster3.vendor
