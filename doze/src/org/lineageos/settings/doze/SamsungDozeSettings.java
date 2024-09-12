92debc3d51f696e2ad09d4e819cf3a3756f32b69
1bdellatif <ptaoubi@gmail.com>
2024-09-13 00:07:07 +0100

    doze

--- doze/src/org/lineageos/settings/doze/SamsungDozeSettings.java
+++ doze/src/org/lineageos/settings/doze/SamsungDozeSettings.java
@@ -17,31 +17,28 @@
 
 package org.lineageos.settings.doze;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.TextView;
 
 import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 import org.lineageos.internal.util.ScreenType;
 
 public class SamsungDozeSettings extends PreferenceFragment
-        implements CompoundButton.OnCheckedChangeListener, Preference.OnPreferenceChangeListener {
+        implements OnPreferenceChangeListener, OnMainSwitchChangeListener {
 
-    private TextView mTextView;
-    private View mSwitchBar;
+    private MainSwitchPreference mSwitchBar;
 
     private SwitchPreference mAlwaysOnDisplayPreference;
     private SwitchPreference mHandwavePreference;
+    private SwitchPreference mPickUpPreference;
     private SwitchPreference mPocketPreference;
     private SwitchPreference mWakeOnGesturePreference;
 
@@ -50,10 +47,12 @@ public class SamsungDozeSettings extends PreferenceFragment
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.gesture_panel);
-        final ActionBar actionBar = getActivity().getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
 
         boolean dozeEnabled = Utils.isDozeEnabled(getActivity());
+
+        mSwitchBar = (MainSwitchPreference) findPreference(Utils.DOZE_ENABLE);
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(dozeEnabled);
 
         mAlwaysOnDisplayPreference = findPreference(Utils.ALWAYS_ON_DISPLAY);
         mAlwaysOnDisplayPreference.setEnabled(dozeEnabled);
@@ -63,6 +62,10 @@ public class SamsungDozeSettings extends PreferenceFragment
         mHandwavePreference = findPreference(Utils.GESTURE_HAND_WAVE_KEY);
         mHandwavePreference.setEnabled(dozeEnabled);
         mHandwavePreference.setOnPreferenceChangeListener(this);
+
+        mPickUpPreference = findPreference(Utils.GESTURE_PICK_UP_KEY);
+        mPickUpPreference.setEnabled(dozeEnabled);
+        mPickUpPreference.setOnPreferenceChangeListener(this);
 
         mPocketPreference = findPreference(Utils.GESTURE_POCKET_KEY);
         mPocketPreference.setEnabled(dozeEnabled);
@@ -77,6 +80,7 @@ public class SamsungDozeSettings extends PreferenceFragment
             getPreferenceScreen().removePreference(mAlwaysOnDisplayPreference);
         } else {
             mHandwavePreference.setDependency(Utils.ALWAYS_ON_DISPLAY);
+            mPickUpPreference.setDependency(Utils.ALWAYS_ON_DISPLAY);
             mPocketPreference.setDependency(Utils.ALWAYS_ON_DISPLAY);
             mWakeOnGesturePreference.setDependency(Utils.ALWAYS_ON_DISPLAY);
         }
@@ -93,36 +97,6 @@ public class SamsungDozeSettings extends PreferenceFragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        final View view =
-                LayoutInflater.from(getContext()).inflate(R.layout.doze, container, false);
-        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        boolean dozeEnabled = Utils.isDozeEnabled(getActivity());
-
-        mTextView = view.findViewById(R.id.switch_text);
-        mTextView.setText(getString(dozeEnabled ?
-                R.string.switch_bar_on : R.string.switch_bar_off));
-
-        mSwitchBar = view.findViewById(R.id.switch_bar);
-        Switch switchWidget = mSwitchBar.findViewById(android.R.id.switch_widget);
-        switchWidget.setChecked(dozeEnabled);
-        switchWidget.setOnCheckedChangeListener(this);
-        mSwitchBar.setActivated(dozeEnabled);
-        mSwitchBar.setOnClickListener(v -> {
-            switchWidget.setChecked(!switchWidget.isChecked());
-            mSwitchBar.setActivated(switchWidget.isChecked());
-        });
-    }
-
-    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (Utils.ALWAYS_ON_DISPLAY.equals(preference.getKey())) {
             Utils.enableAlwaysOn(getActivity(), (Boolean) newValue);
@@ -134,12 +108,11 @@ public class SamsungDozeSettings extends PreferenceFragment
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
         Utils.enableDoze(getActivity(), isChecked);
         Utils.checkDozeService(getActivity());
 
-        mTextView.setText(getString(isChecked ? R.string.switch_bar_on : R.string.switch_bar_off));
-        mSwitchBar.setActivated(isChecked);
+        mSwitchBar.setChecked(isChecked);
 
         if (!isChecked) {
             Utils.enableAlwaysOn(getActivity(), false);
@@ -148,16 +121,8 @@ public class SamsungDozeSettings extends PreferenceFragment
         mAlwaysOnDisplayPreference.setEnabled(isChecked);
 
         mHandwavePreference.setEnabled(isChecked);
+        mPickUpPreference.setEnabled(isChecked);
         mPocketPreference.setEnabled(isChecked);
         mWakeOnGesturePreference.setEnabled(isChecked);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            getActivity().onBackPressed();
-            return true;
-        }
-        return false;
     }
 }
