f1a14cb56dc743c0a8dc35c14f5833d7d3486d26
1bdellatif <ptaoubi@gmail.com>
2025-01-03 14:18:21 +0100

    proprietary files

--- extract-files.py
+++ extract-files.py
@@ -14,6 +14,9 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
+import extract_utils.tools
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_9'
+
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib/libsec-ril.so': blob_fixup()
         .replace_needed('libhidltransport.so', 'libcutils-v29.so')
@@ -28,13 +31,13 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libshim_binder.so'),
     'vendor/lib/libarcsoft_nighthawk.so': blob_fixup()
         .fix_soname(),
-#    ('vendor/bin/thermal-engine', 'vendor/lib/libmmcamera2_sensor_modules.so'): blob_fixup()
-#        .('/system/etc', r'/vendor/etc'),
-#    ('vendor/bin/mpdecision', 'vendor/lib/libsam.so'): blob_fixup()
-#        .('/data/system', r'/data/vendor'),
-#    'vendor/lib/libmmcamera2_sensor_modules': blob_fixup()
-#        .('/system/lib', r'/vendor/lib')
-#        .('/system/cameradata', r'vendor/cameradata'),
+    ('vendor/bin/thermal-engine', 'vendor/lib/libmmcamera2_sensor_modules.so'): blob_fixup()
+        .sig_replace('2F 73 79 73 74 65 6D 2F 65 74 63', '2F 76 65 6E 64 6F 72 2F 65 74 63'),
+    ('vendor/bin/mpdecision', 'vendor/lib/libsam.so'): blob_fixup()
+        .sig_replace('2F 64 61 74 61 2F 73 79 73 74 65 6D', '2F 64 61 74 61 2F 76 65 6E 64 6F 72'),
+    'vendor/lib/libmmcamera2_sensor_modules': blob_fixup()
+        .sig_replace('2F 73 79 73 74 65 6D 2F 6C 69 62', '2F 76 65 6E 64 6F 72 2F 6C 69 62')
+        .sig_replace('2F 73 79 73 74 65 6D 2F 63 61 6D 65 72 61 64 61 74 61', '2F 76 65 6E 64 6F 72 2F 63 61 6D 65 72 61 64 61 74 61'),
 }
 
 module = ExtractUtilsModule(
